b2663d7c5900f7469cc7bedcaf45630bc59f10fa
nattts <nsssttts@gmail.com>
2025-11-02 00:42:37 +0200

    updated libs versions + additional logs @activity

--- app/src/main/java/com/example/custom_keyboard/MainActivity.kt
+++ app/src/main/java/com/example/custom_keyboard/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.custom_keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.RadioGroup
+import android.widget.RadioButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val selectedRadioButton = findViewById<RadioButton>(checkedId)
+            println("checkedId => ${selectedRadioButton.text}")
+        }
     }
 }
